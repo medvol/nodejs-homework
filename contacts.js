24c66b40a7ee277b7e3494f62dc65f699c6428bd
Oleh Medvid <meandjob@protonmail.com>
2022-11-23 23:11:41 +0200

    create project

---
+++ contacts.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
