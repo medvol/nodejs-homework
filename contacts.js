cc6478fb8cd466bfb536cb2efbba62e40d094e28
Oleh Medvid <meandjob@protonmail.com>
2022-11-26 01:21:58 +0200

    add: CLI application logic

--- contacts.js
+++ contacts.js
@@ -1,28 +1,58 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
 
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const contactsPath = path.join(__dirname, "/db", "contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твій код
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts(contactId);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (!contact) {
+      return null;
+    }
+    return contact;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твій код
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(contact=> contact.id === contactId);
+    if (idx === -1) {
+      return null;
+    }
+    const updateContacts = contacts.filter((_, index) => index !== idx);
+    await fs.writeFile(contactsPath, JSON.stringify(updateContacts));
+    return contacts [idx]
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твій код
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: v4(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
-
 
 module.exports = {
   listContacts,
